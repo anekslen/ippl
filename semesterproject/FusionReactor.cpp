7a956c56605cf20574b4899bc2d562894d8f9b28
Anna <anna.hutter9444@gmail.com>
2025-04-23 12:56:13 +0200

    Add path to outputfolder

--- semesterproject/FusionReactor.cpp
+++ semesterproject/FusionReactor.cpp
@@ -1,14 +1,18 @@
 // Fusion Reactor simulation
 //   Usage:
-//     ./FusionReactor <Np> <Nt> <t_method> <grid_file_name> --info 10
+//     ./FusionReactor <Np> <Nt> <step_method> <grid_file_name> <input_particles_file_name> <dt> <output_folder> <writeData> --info 10
 
 //     Np       = Total no. of particles in the simulation
 //     Nt       = Number of time steps
-//     t_method = Time-stepping method used e.g. Leapfrog
+//     step_method = Time-stepping method used e.g. Leapfrog
 //     grid_file_name = path to the grid file in VTK format
+//     input_particles_file_name = path to the file containing the initial particle positions
+//     dt      = time step size
+//     output_folder = path to the folder where the output files will be written
+//     writeData = true/false if the data should be written for each time step not only the last position of each particle
 
 //     Example:
-//     ./FusionReactor 1 20 LeapFrog reactormesh/mesh.vtk reactormesh/InitialParticles.csv --info 10
+//     ./FusionReactor 1 20 Boris reactormesh/mesh.vtk reactormesh/InitialParticles.csv 1e-8 ./data true --info 10
 
 constexpr unsigned Dim = 3;
 using T                = double;
@@ -48,11 +52,34 @@ int main(int argc, char* argv[]) {
         std::string step_method = argv[arg++];
         const char* grid_filename = argv[arg++];
         const char* particles_filename = argv[arg++];
-        const char* output_folder = argv[arg++];
         double dt = std::atof(argv[arg++]);
+        
+        const char* output_folder;
+        if (argc > arg) {
+            output_folder = argv[arg++];
+            std::cout << "output_folder: " << output_folder << std::endl;
+        } else {
+            output_folder = "./data";
+        }
+
+        bool writeData;
+        if (argc > arg) {
+            std::string boolStr = argv[arg++];
+            std::transform(boolStr.begin(), boolStr.end(), boolStr.begin(), ::tolower);
+            if (boolStr == "true" || boolStr == "1") {
+                writeData = true;
+            } else if (boolStr == "false" || boolStr == "0") {
+                writeData = false;
+            } else {
+                std::cerr << "Invalid boolean value: " << boolStr << ". Use 'true'/'false' or '1'/'0'.\n";
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            writeData = false;
+        }
 
         // Create an instance of a manger for the considered application
-        FusionReactorManager<T, Dim> manager(totalP, nt, step_method, output_folder, dt);
+        FusionReactorManager<T, Dim> manager(totalP, nt, step_method, output_folder, dt, writeData);
 
         // Perform pre-run operations, including creating mesh, particles,...
         manager.pre_run(grid_filename, particles_filename);
